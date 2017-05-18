dd700c39cd81d9659c0db982a81a43fb5b265bd3
llmanuel <llauromanuel@gmail.com>
2017-05-18 16:20:06 -0300

    Creo clase Map para conocer donde estan los objetos en el mapa y sus distintos terrenos

--- src/map.cpp
+++ src/map.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "map.h"
+
+Map::Map(int x, int y, int width, int height,
+   std::vector<std::vector<Cell>>& terrain_map) : map_size(x,y,width,height),
+    terrain_map(terrain_map){}
+
+int Map::getTerrainFactorOn(int x, int y) {
+    int w_cell = terrain_map[0][0].getWidthOfCell();
+    int x_pos = x / w_cell;
+    int y_pos = y / w_cell;
+
+    return terrain_map[x_pos][y_pos].getMovementFactor();
+}
+
+bool Map::areThisPointsEmpty(Size size) {
+
+    // Check on all Occupant vector if
+    // this size collapse width thier own
+
+    return true;
+}
+
+int Map::getWidth() {
+    return map_size.getWidth();
+}
+
+int Map::getHeigth() {
+    return map_size.getHeight();
+}
--- src/map.h
+++ src/map.h
@@ -5,9 +5,35 @@
 #ifndef Z_TPGRUPAL_MAP_H
 #define Z_TPGRUPAL_MAP_H
 
+#include <vector>
+#include "cell.h"
+#include "Occupant.h"
+#include "unit.h"
 
-class map {
+// later written
+class Unit;
 
+class Map {
+private:
+    std::vector<std::vector<Cell>> terrain_map;
+    Size map_size;
+    std::vector<Occupant> all_occupants;
+    std::vector<Unit> all_units;
+
+public:
+    // Map receives the center position (x,y) and dimensions width and height
+    Map(int x, int y, int width, int height,
+        std::vector<std::vector<Cell>>& terrain_map);
+
+    // Recieves the coordinates (x,y) and returns the terrain factor on that
+    // position on the map.
+    int getTerrainFactorOn(int x, int y);
+
+    bool areThisPointsEmpty(Size size);
+
+    int getWidth();
+
+    int getHeigth();
 };
 
 
