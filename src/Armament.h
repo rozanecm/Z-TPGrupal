098bb3a9f92a4da6ff231c9234d799537d281bbe
Matias Rozanec <rozanecm@gmail.com>
2017-05-21 11:44:06 -0300

    create classes needed in client.

---
+++ src/Armament.h
@@ -0,0 +1,22 @@
+#ifndef Z_TPGRUPAL_ARMAMENT_H
+#define Z_TPGRUPAL_ARMAMENT_H
+
+
+#include <SDL_mixer.h>
+#include <vector>
+#include <string>
+#include <map>
+#include <gtkmm/drawingarea.h>
+
+class Armament {
+private:
+    /* sound used when shot */
+    Mix_Chunk* sound;
+
+    /* animation is stored as a series of images needed
+     * to draw the animation. */
+    std::vector<Glib::RefPtr<Gdk::Pixbuf>> animation;
+};
+
+
+#endif //Z_TPGRUPAL_ARMAMENT_H
