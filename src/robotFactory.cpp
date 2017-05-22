5035477de04466fbcaf7d680126ef03914694cc5
llmanuel <llauromanuel@gmail.com>
2017-05-22 10:10:00 -0300

    Creación de clase RobotFactory que se encargan de guardar los moldes de los robot e ir creandolos

--- src/robotFactory.cpp
+++ src/robotFactory.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "robotFactory.h"
+
+RobotFactory::RobotFactory(int life, std::string type, Size position,
+                           std::vector<UnitMold> units) :
+        Factory(life,type, position), units(units){
+    it = units.begin();
+}
+
+void RobotFactory::build() {
+    this->running = true;
+
+    while (running) {
+    // after sleeping time expected
+    // create the correct amount of units
+    }
+}
+
+void RobotFactory::stopBuilding() {
+    this->running = false;
+}
+
+int RobotFactory::getSelectedUnitTime() {
+    return it->getCreationTime();
+}
+
+std::string RobotFactory::nextUnit() {
+    if ( ++it == units.end())
+        it = units.begin();
+    return it->getTypeOfUnit();
+}
+
+Occupant *RobotFactory::destroyFactory() {
+    Occupant* ruins = new Occupant(1000,"Ruins",this->occ_size);
+    return ruins;
+}
--- src/robotFactory.h
+++ src/robotFactory.h
@@ -6,8 +6,31 @@
 #define Z_TPGRUPAL_ROBOTFACTORY_H
 
 
-class robotFactory {
+#include "unitMold.h"
+#include "factory.h"
 
+class RobotFactory: public Factory {
+private:
+    std::vector<UnitMold> units;
+    std::vector<UnitMold>::iterator it;
+
+public:
+    RobotFactory(int life, std::string type, Size position,
+                                    std::vector<UnitMold> units);
+
+    // starts the creation of the selected unit
+    void build();
+
+    // Stops the creation of units
+    void stopBuilding();
+
+    // Returns the creational time of the selected Unit
+    int getSelectedUnitTime();
+
+    // Returns the type of the unit that now is selected
+    std::string nextUnit();
+
+    Occupant* destroyFactory();
 };
 
 
