8b45a84dd61477fd232bd404b41badc236a1beff
llmanuel <llauromanuel@gmail.com>
2017-05-22 00:04:01 -0300

    Creacion de ToughtType class para almacenar los valores configurables de la unidad tipo Tough y encangarce de la creación del mismo

---
+++ src/toughType.cpp
@@ -0,0 +1,25 @@
+//
+// Created by rodian on 21/05/17.
+//
+
+#include "toughType.h"
+
+ToughType::ToughType(int tec_level, int life, int range, int width, int height,
+                     int unit_speed, int fire_rate, int time) :
+tec_level(tec_level), life(life), range(range), width(width), height(height),
+unit_speed(unit_speed), fire_rate(fire_rate), creation_time(time) {}
+
+Unit *ToughType::createTought(int x, int y, Map& map) {
+    Size unit_size(x,y,width,height);
+    Size unit_range(x,y,range,range);
+    Unit* new_tought = new Unit(life,"Tough", 4,unit_size,unit_range,map);
+    return new_tought;
+}
+
+int ToughType::getTechnologyLevel() const {
+    return tec_level;
+}
+
+int ToughType::getCreationTime() {
+    return creation_time;
+}
