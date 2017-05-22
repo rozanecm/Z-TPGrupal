2342c0eaf5f09734436d85f4627c78036a19d3f2
llmanuel <llauromanuel@gmail.com>
2017-05-22 09:26:23 -0300

    Creación de UnitMold class para almacenar los datos de los distintos robots y crear nuevas unidades

--- src/unitMold.h
+++ src/unitMold.h
@@ -6,8 +6,28 @@
 #define Z_TPGRUPAL_UNITMOLD_H
 
 
-class unitMold {
+#include "unit.h"
 
+class UnitMold {
+private:
+    int tec_level, life, range, width, height, unit_speed, fire_rate,
+            creation_time;
+    std::string type;
+
+public:
+    UnitMold(int tec_level, int life, int range, int width, int height,
+           int unit_speed, int fire_rate, int creation_time, std::string type);
+
+    Unit* createUnit(int x, int y, Map& map);
+
+    // Returns the technology level needed to create Tough unit
+    int getTechnologyLevel() const;
+
+    // Returns creation time in seconds
+    int getCreationTime();
+
+    // Returns the type of unit that this mold creates
+    std::string getTypeOfUnit() const;
 };
 
 
