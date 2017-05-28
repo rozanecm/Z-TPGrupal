8f15dd825ce930534ecfa2cd78c5f8ecfed18c51
Matias Rozanec <rozanecm@gmail.com>
2017-05-28 19:25:52 -0300

    create basic structure in camera.

--- src/client/Camera.cpp
+++ src/client/Camera.cpp
@@ -1,1 +1,16 @@
 #include "Camera.h"
+
+#define NUMBER_OF_TILES_TO_SHOW 10
+
+Camera::Camera(unsigned int tileSize, unsigned int mapWidth,
+               unsigned int mapHeight) :
+        tileSize(tileSize),
+        minXCoordinate(NUMBER_OF_TILES_TO_SHOW*tileSize/2),
+        minYCoordinate(NUMBER_OF_TILES_TO_SHOW*tileSize/2),
+        maxXCoordinate(mapWidth*tileSize-NUMBER_OF_TILES_TO_SHOW*tileSize/2),
+        maxYCoordinate(mapWidth*tileSize-NUMBER_OF_TILES_TO_SHOW*tileSize/2){
+}
+
+std::pair<unsigned int, unsigned int> Camera::getPosition() {
+    return position;
+}
--- src/client/Camera.h
+++ src/client/Camera.h
@@ -2,8 +2,23 @@
 #define Z_TPGRUPAL_CAMERA_H
 
 
-class Camera {
+#include <utility>
 
+class Camera {
+public:
+    Camera(unsigned int tileSize, unsigned int mapWidth,
+               unsigned int mapHeight);
+
+    std::pair<unsigned int, unsigned int> getPosition();
+private:
+    /* save tile size in pixels so calculations on max and min coord.
+     * can be done */
+    unsigned int tileSize;
+
+    /* position in pixels */
+    std::pair<unsigned int, unsigned int> position;
+
+    unsigned int minXCoordinate, minYCoordinate, maxXCoordinate, maxYCoordinate;
 };
 
 
