fe5fe1a4fea1e5a77e25f6268716a50d2eb51dd8
llmanuel <llauromanuel@gmail.com>
2017-05-29 13:46:39 -0300

    agrego class lobby para que distintos jugadores se unan para empezar una partida

---
+++ src/server/lobby.cpp
@@ -0,0 +1,23 @@
+//
+// Created by rodian on 29/05/17.
+//
+
+#include "lobby.h"
+
+Lobby::Lobby(int id) : lobby_id(id), all_ready(false) {}
+
+void Lobby::startGame() {
+    if(all_ready){
+        //start game
+    }
+}
+
+void Lobby::ready() {
+    all_ready = true;
+}
+
+bool Lobby::addPlayer(Player &player) {
+    if(players.size() < 4)
+        players.push_back(player);
+    return (players.size() < 4);
+}
