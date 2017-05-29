0c35a0b8d56057a9736444a3a2a9755bca258078
Matias Rozanec <rozanecm@gmail.com>
2017-05-29 15:05:27 -0300

    complete constructor. Add Oxygen-type documentation.

--- src/client/Camera.cpp
+++ src/client/Camera.cpp
@@ -1,14 +1,23 @@
 #include "Camera.h"
+#include <utility>
 
-Camera::Camera(unsigned int tileSize, unsigned int mapWidth, unsigned int mapHeight,
+Camera::Camera(unsigned int tileSize, unsigned int mapWidth,
+               unsigned int mapHeight,
                unsigned int numberOfTilesToShow) :
-        tileSize(tileSize), numberOfTilesToShow(numberOfTilesToShow),
+        tileSize(tileSize),
+        numberOfTilesToShow(numberOfTilesToShow),
         minXCoordinate(numberOfTilesToShow*tileSize/2),
         minYCoordinate(numberOfTilesToShow*tileSize/2),
         maxXCoordinate(mapWidth*tileSize-numberOfTilesToShow*tileSize/2),
-        maxYCoordinate(mapHeight*tileSize-numberOfTilesToShow*tileSize/2){
+        maxYCoordinate(mapHeight*tileSize-numberOfTilesToShow*tileSize/2),
+        position(numberOfTilesToShow*tileSize/2,
+                 numberOfTilesToShow*tileSize/2){
 }
 
+/**
+ *
+ * @return camera position is given in pixels.
+ */
 std::pair<unsigned int, unsigned int> Camera::getPosition() {
     return position;
 }
