035a21fe73bdeb63b95189bd7f3c35166738cfce
Lucas L <lucaslavandeira@gmail.com>
2017-06-04 12:00:32 -0300

    Remove .c files from cpplint

--- src/cpplint.sh
+++ src/cpplint.sh
@@ -1,1 +1,1 @@
-python ./cpplint.py --extensions=h,hpp,c,cpp --filter=`cat filter_options` `find -regextype posix-egrep -regex '.*\.(h|hpp|c|cpp)'`
+python ./cpplint.py --extensions=h,cpp --filter=`cat filter_options` `find -regextype posix-egrep -regex '.*\.(h|hpp|c|cpp)'`
