60a7ef751edc5f1dcaabff69b19b52a5d26802ab
Lucas L <lucaslavandeira@gmail.com>
2017-06-10 12:35:01 -0300

    Add cfg file; read port from cfg

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
-#include "cell.h"
-#include "compass.h"
+#include <pugixml.hpp>
 #include "server.h"
 
 #define PORT 8000
 
+unsigned int load_port(const char* cfg_file_path) {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(cfg_file_path);
+    if (!result) {
+        std::cout << "Error reading cfg file: " << result.description()
+                  << std::endl;
+        return 0;
+    }
+    pugi::xml_node port_node = doc.child("Config").child("Port");
+    std::string port = port_node.attribute("port").value();
+    if (!port.size()) {
+        std::cout << "Error reading port from cfg file!" << std::endl;
+        return 0;
+    }
+    int a = std::stoi(port);
+    return (unsigned int) a;
+}
+
 int main (int argc, char **argv) {
+    std::string cfg = argv[1];
+    if (!cfg.size()) {
+        std::cout << "Error loading cfg file" << std::endl;
+        return 1;
+    }
+    unsigned int port = load_port(argv[1]);
+    if (!port) {
+        return 1;
+    }
 
     try {
         std::mutex m;
 
         Menu menu(m);
 
-        Server server_accepter(8000, menu);
-
+        Server server_accepter(port, menu);
         server_accepter.start();
-
         char exit = 'a';
 
         while (exit != 'q') {
