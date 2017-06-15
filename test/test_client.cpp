c5ac0dd45ff99222380fd67c309c74b4b4e0209e
llmanuel <llauromanuel@gmail.com>
2017-06-15 13:55:30 -0300

    modificaciones en cliente dummy para crear una partida directamente

--- test/test_client.cpp
+++ test/test_client.cpp
@@ -5,6 +5,17 @@
 int main() {
     Socket s("127.0.0.1", 8000);
     Messenger m(s);
+    std::string msg1 = "create-lobby";
+    m.sendMessage(msg1);
+    std::cout << "Response: " << m.recieveMessage() << std::endl;
+
+    msg1 = "ready";
+    m.sendMessage(msg1);
+    std::cout << "Response: " << m.recieveMessage() << std::endl;
+
+    msg1 = "start-game";
+    m.sendMessage(msg1);
+    std::cout << "Response: " << m.recieveMessage() << std::endl;
     while(true) {
         std::cout << "Enter message: " << std::endl << ">> ";
         std::string msg;
