152c6791cca2a7ec3d172ebaa3face6bc347d164
Lucas L <lucaslavandeira@gmail.com>
2017-06-17 22:34:22 -0300

    add server load commands

---
+++ src/client/commands/AddBuilding.cpp
@@ -0,0 +1,8 @@
+#include "AddBuilding.h"
+
+AddBuilding::AddBuilding(BuildingsMonitor &buildings) : buildings(buildings) {
+
+}
+
+void AddBuilding::execute(const std::vector<std::string> &args) {
+}
